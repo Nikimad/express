2d480fe03ecb480fff33524cc5bee98d33a18442
Nikimad <PybePoiD@gmail.com>
2023-06-22 14:31:21 +0300

    change

--- api/index.js
+++ api/index.js
@@ -1,21 +1,21 @@
 import { join, dirname } from "node:path";
 import { fileURLToPath } from "node:url";
-import { LowSync } from 'lowdb'
-import { JSONFileSync } from 'lowdb/node'
+import { Low } from "lowdb";
+import { JSONFile } from "lowdb/node";
 import express from "express";
 import uniqueId from "lodash/uniqueId.js";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const filePath = join(__dirname, "db.json");
 
-const adapter = new JSONFileSync(filePath);
+const adapter = new JSONFile(filePath);
 const defaultData = {
   boards: {
     items: {},
     ids: [],
   },
 };
-const db = new LowSync(adapter, defaultData);
+const db = new Low(adapter, defaultData);
 
 const app = express();
 const port = 3000;
@@ -26,19 +26,20 @@ app.get("/", (req, res) => {
   res.send("Demo server");
 });
 
-app.get("/boards", (req, res) => {
-  db.read();
+app.get("/boards", async (req, res) => {
+  await db.read();
   res.send(db.data.boards);
 });
 
 app.post("/boards", async (req, res) => {
-  db.read();
+  await db.read();
   const id = uniqueId();
   const entity = { id, ...req.body };
   db.data.boards.items[id] = entity;
   db.data.boards.ids = [id, ...db.data.boards.ids];
-  db.write();
-  res.json(entity);
+  await db.write();
+  await db.read();
+  res.json(db.data);
 });
 
 app.listen(port);
