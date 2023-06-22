c574e117ee64195d9c96c634571bd4ac1c284a63
Nikimad <PybePoiD@gmail.com>
2023-06-22 14:47:20 +0300

    change

--- api/index.js
+++ api/index.js
@@ -1,18 +1,21 @@
-import { LowSync } from "lowdb";
-import { LocalStorage } from 'lowdb/browser'
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { Low } from "lowdb";
+import { JSONFile } from "lowdb/node";
 import express from "express";
 import uniqueId from "lodash/uniqueId.js";
 
-localStorage.setItem("db");
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filePath = join(__dirname, "db.json");
 
+const adapter = new JSONFile(filePath);
 const defaultData = {
   boards: {
     items: {},
     ids: [],
   },
 };
-
-const db = new LowSync(new LocalStorage("db"), defaultData);
+const db = new Low(adapter, defaultData);
 
 const app = express();
 const port = 3000;
@@ -24,8 +27,19 @@ app.get("/", (req, res) => {
 });
 
 app.get("/boards", async (req, res) => {
-  db.read();
+  await db.read();
   res.send(db.data.boards);
+});
+
+app.post("/boards", async (req, res) => {
+  await db.read();
+  const {items, ids} = db.data;
+  const id = uniqueId();
+  const entity = { id, ...req.body };
+  items[id] = entity;
+  ids = [id, ...ids];
+  await db.write();
+  res.json(entity);
 });
 
 app.listen(port);
